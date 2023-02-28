c424f25fc3aa1f7e560fe1d40181f26e0e1c3f3b
andrepacifico <124262197+andrepacifico@users.noreply.github.com>
2023-02-28 10:27:49 -0300

    update

---
+++ Aula4 - Condições/ex01/ex01.js
@@ -0,0 +1,13 @@
+var vel = 80
+
+console.log(`Você está andando a ${vel}km/h`)
+console.log(`A velocidade permitida da pista é de 60km/h`)
+
+if (vel >= 61) {
+    console.log(`Você será multado`)
+
+}
+
+else (vel <= 60); {
+    console.log(`Você não foi multado`)
+}
