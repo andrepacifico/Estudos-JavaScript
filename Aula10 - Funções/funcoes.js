15f163b1a2420bee68670b03a22ccf31c98545a3
andrepacifico <124262197+andrepacifico@users.noreply.github.com>
2023-03-06 19:40:21 -0300

    exercícios sobre funções, fatorial etc

---
+++ Aula10 - Funções/funcoes.js
@@ -0,0 +1,16 @@
+function parouimpar(n) {
+    if (n%2 == 0) {
+        return 'par'
+    } else {
+        return 'impar'
+    }
+
+}
+
+
+
+let res = parouimpar()
+    console.log(res)
+
+    //o resultado também pode ser mostrado da forma
+    //console.log(parouimpar(n))
